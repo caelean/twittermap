2c05e5e90639f8199001030f3126499a974cb4bb
Caelean Barnes <caeleanb@gmail.com>
2017-10-23 19:52:31 +0200

    Script returns accurate location of twitter handle

--- TwitterInfo.py
+++ TwitterInfo.py
@@ -1,6 +1,7 @@
 from twython import Twython
 import requests
 import json
+import sys
 
 f = open('.keys.txt', 'r')
 CONSUMER_KEY = f.readline().strip('\n\r')
@@ -11,11 +12,13 @@ f.close()
 
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
-user = twitter.show_user(screen_name='@realdonaldtrump', include_entities=False)
+user = twitter.show_user(screen_name=sys.argv[1], include_entities=False)
 print('user     : ' + user['screen_name'])
 print('location : ' + user['location'])
-
-r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCZBUXlJTya934fLCVMwtZZ_UfyYpyx6_8")
+query = user['location']
+key = "&key=AIzaSyCZBUXlJTya934fLCVMwtZZ_UfyYpyx6_8"
+url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query + key
+r = requests.get(url)
 
 print('latitude : ' + str(json.loads(r.content)['results'][0]['geometry']['location']['lat']))
 print('longitude: ' + str(json.loads(r.content)['results'][0]['geometry']['location']['lng']))
